61316abc3a82f1476673b30e4a949c0451d0569f
MERIDIO-K <34624593+MERIDIO-K@users.noreply.github.com>
2021-05-01 23:37:09 +0200

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head> 
-<title> Comment utiliser un code PHP dans une page HTML </title>
-</head>
-<body>
-Bonjour LWS, aujourd'hui c'est le <?php setlocale (LC_TIME, 'fr_FR.utf8','fra'); echo (strftime("%A %d %B %Y")) ?> 
-</body>
-</html>
